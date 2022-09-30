2d921dc389a165baa958b1fd6fd17738a83fdf50
tamas.molnar <tamas.molnar@liferay.com>
2022-09-30 14:43:41 +0200

    5. Imports and constants

--- src/TenderlySimulation.jsx
+++ src/TenderlySimulation.jsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const TENDERLY_USER = process.env.REACT_APP_TENDERLY_USER;
+const TENDERLY_PROJECT = process.env.REACT_APP_TENDERLY_PROJECT;
+const TENDERLY_ACCESS_KEY = process.env.REACT_APP_TENDERLY_ACCESS_KEY;
+
+const SIMULATE_URL = `https://api.tenderly.co/api/v1/account/${TENDERLY_USER}/project/${TENDERLY_PROJECT}/simulate`;
+const OPTS = {
+  headers: {
+    'X-Access-Key': TENDERLY_ACCESS_KEY
+  }
+}
+
 export default function TenderlySimulation({}) {
 	return (
 		<div>
